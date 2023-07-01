09e97b122276262e47f4da6570680a6676ef15b0
Danny-Cisco <gumballelectronics@gmail.com>
2023-07-02 00:59:53 +1000

    axios

--- api/narakeet.js
+++ api/narakeet.js
@@ -1,36 +1,27 @@
-const fetch = require('node-fetch');
-const { pipeline } = require('stream/promises');
-const { createWriteStream } = require('fs');
-const { Readable } = require('stream');
-const { promisify } = require('util');
-const streamPipeline = promisify(pipeline);
+const axios = require('axios');
+const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = async (req, res) => {
     const APIKEY = process.env.NARAKEET_API_KEY;
-    const voice = 'mike';
+    const voice = 'Mike';
     const text = req.body.text;
 
     try {
-        const response = await fetch(
-            `https://api.narakeet.com/text-to-speech/mp3?voice=${voice}`,
-            {
-                method: 'POST',
-                headers: {
-                    'accept': 'application/octet-stream',
-                    'x-api-key': APIKEY,
-                    'content-type': 'text/plain'
-                },
-                body: text
-            }
-        );
-        
-        if (!response.ok) {
-            throw new Error(`Server response: ${response.statusText}`);
-        }
+        const response = await axios({
+            method: 'post',
+            url: `https://api.narakeet.com/text-to-speech/mp3?voice=${voice}`,
+            headers: {
+                'accept': 'application/octet-stream',
+                'x-api-key': APIKEY,
+                'content-type': 'text/plain'
+            },
+            data: text,
+            responseType: 'stream'
+        });
 
-        await streamPipeline(response.body, createWriteStream('result.mp3'));
+        response.data.pipe(fs.createWriteStream('result.mp3'));
         res.status(200).send('Audio created successfully');
     } catch (error) {
         res.status(500).send(`Server error: ${error.message}`);
